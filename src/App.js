17e72b0075cd233d20bb0543bb08c25553898a22
Sonu Kumar <sonu.kumar@teliolabs.com>
2023-03-30 19:42:56 +0530

    home navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
+import About from './pages/About';
+import Articles from './pages/Articles';
+import Home from './pages/Home';
+import {React} from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <h1>Server-Side rendering example</h1>
+      <ul>
+        <li>
+          <Link to={'/'}>Home</Link>
+        </li>
+        <li>
+          <Link to={'/about'} path="about">About</Link>
+        </li>
+        <li>
+          <Link to={'/articles'} path="articles">Articles</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" exact Component={Home} />
+        <Route path="/about" exact Component={About} />
+        <Route path="/articles" exact Component={Articles} />
+          
+       
+       
+      </Routes>
+    </BrowserRouter>
   );
 }
 
